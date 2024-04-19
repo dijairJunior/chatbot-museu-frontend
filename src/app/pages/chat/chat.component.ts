d1f9320c1a633098d11f4b795b776d296a19c4c1
Dijair Camargo <dijair.camargo@gmail.com>
2024-04-19 13:37:16 -0300

    feat: finalization of the chatbot front end, and testing on an api

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -5,6 +5,8 @@ import { ChatSuggestionsComponent } from '../../components/chat-suggestions/chat
 import { CommonModule } from '@angular/common';
 import { Message } from '../../types/message.type';
 import { ChatDialogComponent } from '../../components/chat-dialog/chat-dialog.component';
+import { MessageService } from '../../services/message.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-chat',
@@ -16,17 +18,43 @@ import { ChatDialogComponent } from '../../components/chat-dialog/chat-dialog.co
     ChatSuggestionsComponent,
     ChatDialogComponent
   ],
+
+  providers: [
+    MessageService
+  ],
+
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 
 export class ChatComponent {
-  messages: Message[] = []
+  messages: Message[] = JSON.parse(localStorage.getItem("messages") ?? "[]")
+
+  constructor(private service: MessageService) {}
+
+  updateLocalStoarage() {
+    localStorage.setItem("messages", JSON.stringify(this.messages))
+  }
 
   sendSuggestedQuestion(question: string){
     this.messages.push({
       type: 'request',
       message: question
     })
-  }  
+
+    this.updateLocalStoarage()
+    this.sendMessage(question)
+  }
+  
+  sendMessage(message: string) {
+    this.service.send(message).subscribe({
+      next: (body) => {
+        this.messages.push({
+          type: "response",
+          message: body.response
+        })
+        this.updateLocalStoarage()
+      }
+    })
+  }
 }
